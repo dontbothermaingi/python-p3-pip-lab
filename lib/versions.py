54dbc9562f42068a837270851af0b00ba0c4bed9
+ <+>
2024-03-18 11:21:38 +0300

    Passed all tests

--- lib/versions.py
+++ lib/versions.py
@@ -5,8 +5,10 @@ import pytest
 def python_version():
     return sys.version_info
 
+
 def requests_version():
     return requests.__version__
+
 
 def pytest_version():
     return pytest.__version__
